58d1175eb367322fc24584e12db5b98526d735a4
Ryan Bass <theryanbass@gmail.com>
2022-03-11 21:30:44 -0800

    add client hashset

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,5 +7,12 @@ namespace HairSalon.Models
     public int StylistId {get; set;}
     public string Name {get; set;}
     public string Description {get; set;}
+
+  public virtual ICollection<Client> Clients {get; set;}
+
+    public Stylist()
+    {
+      Clients = new HashSet<Client>() {};
+    }  
   }
 }
