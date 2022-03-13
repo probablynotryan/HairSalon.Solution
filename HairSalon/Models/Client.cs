5fdbbe99287a6d54bb1128f9cdc52fe1a4f7d69a
Ryan Bass <theryanbass@gmail.com>
2022-03-12 17:23:27 -0800

    add phonenumber field to client

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -7,5 +7,6 @@ namespace HairSalon.Models
     public int ClientId {get; set;}
     public string Name {get; set;}
     public int StylistId {get; set;}
+    public string PhoneNumber {get; set;}
   }
 }
