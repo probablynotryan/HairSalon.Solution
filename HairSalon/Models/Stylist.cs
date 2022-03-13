d38fc910d7dbfab2a970c6d7618db86acdde4a40
Ryan Bass <theryanbass@gmail.com>
2022-03-12 17:23:44 -0800

    add position field to stylist

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,6 +7,7 @@ namespace HairSalon.Models
     public int StylistId {get; set;}
     public string Name {get; set;}
     public string Description {get; set;}
+    public string Position {get; set;}
 
   public virtual ICollection<Client> Clients {get; set;}
 
